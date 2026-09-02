233728f079d9143c5269841205b6836e0bfde4ed
MatinMohamadi <matinhma41@gmail.com>
2026-09-02 16:32:17 +0330

    Fix Refresh Token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import (
     AddressDetailView,
@@ -14,6 +15,7 @@ urlpatterns = [
     path("auth/register/", RegisterView.as_view(), name="register"),
     path("auth/login/", LoginView.as_view(), name="login"),
     path("auth/me/", ProfileView.as_view(), name="profile"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("addresses/", AddressListCreateView.as_view(), name="address-list"),
     path("addresses/<int:pk>/", AddressDetailView.as_view(), name="address-detail"),
 ]
